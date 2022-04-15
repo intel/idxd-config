c04bcaa94cb07cefe562915cf7ab0314e668652f
Li Zhang <li4.zhang@intel.com>
2022-04-15 11:01:07 +0800

    accel-config/test: Create new files for placing reused code
    Create accel_test.c and accel_test.h for placing common defines,
    declarations and functions. DSA and IAA test code can share the
    reused code.
    
    Signed-off-by: Li Zhang <li4.zhang@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>

---
+++ test/accel_test.c
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright(c) 2019 Intel Corporation. All rights reserved. */
+#include <accfg/idxd.h>
+#include "accel_test.h"
---
+++ test/accel_test.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2019 Intel Corporation. All rights reserved. */
+#ifndef __ACCEL_TEST_H__
+#define __ACCEL_TEST_H__
+#include <accfg/libaccel_config.h>
+#include <accfg/idxd.h>
+
+#endif
