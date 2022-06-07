15456198315b4851f2bef85c4714959bb4277600
Li Zhang <li4.zhang@intel.com>
2022-06-07 10:58:25 +0800

    accel-config/test: Add test code of operation Zdecompress32
    Add test code of operation Zdecompress32
    
    Signed-off-by: Li Zhang <li4.zhang@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Ramesh Thomas <ramesh.thomas@intel.com>

--- test/iaa_test.c
+++ test/iaa_test.c
@@ -199,6 +199,16 @@ static int test_zcompress(struct acctest_context *ctx, size_t buf_size,
 			if (rc != ACCTEST_STATUS_OK)
 				return rc;
 			break;
+		case IAX_OPCODE_ZDECOMPRESS32:
+			rc = iaa_zdecompress32_multi_task_nodes(ctx);
+			if (rc != ACCTEST_STATUS_OK)
+				return rc;
+
+			/* Verification of all the nodes*/
+			rc = iaa_task_result_verify_task_nodes(ctx, 0);
+			if (rc != ACCTEST_STATUS_OK)
+				return rc;
+			break;
 		default:
 			err("Unsupported op %#x\n", opcode);
 			return -EINVAL;
@@ -299,6 +309,7 @@ int main(int argc, char *argv[])
 	case IAX_OPCODE_ZCOMPRESS16:
 	case IAX_OPCODE_ZDECOMPRESS16:
 	case IAX_OPCODE_ZCOMPRESS32:
+	case IAX_OPCODE_ZDECOMPRESS32:
 		rc = test_zcompress(iaa, buf_size, tflags, opcode, num_desc);
 		if (rc != ACCTEST_STATUS_OK)
 			goto error;
