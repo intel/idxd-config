1d5fda9f51680e6e69cd178a0255e4e1fe28fd65
Ramesh Thomas <ramesh.thomas@intel.com>
2022-08-22 20:13:43 -0700

    accel-config/debian: Change debdch.sh to start log from HEAD
    debdch.sh will generate changelog entries starting from HEAD to previous
    release tag. This allows using the script to update the changelog during
    release time when the tag has yet not been set to the top of tree.
    
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>

--- debdch.sh
+++ debdch.sh
@@ -29,7 +29,7 @@ echo "Adding changes..."
 echo
 
 dch -v $DEB_VERSION --package accel-config -D unstable ""
-cur_release=$(git describe --tags --abbrev=0)
+cur_release=HEAD
 prev_release=$(git describe --tags --abbrev=0 $cur_release^)
 git log $prev_release..$cur_release --pretty=format:"%s" -i -E \
 		--invert-grep --grep=release 2>/dev/null |
