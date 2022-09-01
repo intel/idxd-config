853ca1c5480be3b248e8a273db23e6fd5bdb2d6b
Zhang, Li4 <li4.zhang@intel.com>
2022-09-01 17:51:28 +0800

    accel-config/test: Add file to place algorithms of operation filter
    Add new file iaa_filter.c and iaa_filter.h to place algorithms of
    operation filter and add them into Makefile for compiling.
    
    Signed-off-by: Li Zhang <li4.zhang@intel.com>
    Reviewed-by: Fenghua Yu <fenghua.yu@intel.com>

---
+++ test/algorithms/iaa_filter.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2019 Intel Corporation. All rights reserved. */
+#ifndef _IAA_FILTER_H_
+#define _IAA_FILTER_H_
+
+#include <stdint.h>
+
+#define IAA_FILTER_MAX_DEST_SIZE (2097152 * 2)
+#define IAA_FILTER_MAX_SRC2_SIZE (2097152)
+
+#define IAA_FILTER_AECS_SIZE (32)
+
+struct iaa_filter_flags_t {
+	uint32_t	src1_parse:2;
+	uint32_t	src1_width:5;
+	uint32_t	src2_width:5;
+	uint32_t	src2_bit_order:1;
+	uint32_t	output_width:2;
+	uint32_t	output_bit_order:1;
+	uint32_t	invert_output:1;
+	uint32_t	drop_low_bits:5;
+	uint32_t	drop_high_bits:5;
+	uint32_t	rsvd:5;
+};
+
+struct iaa_filter_aecs_t {
+	uint32_t	rsvd;
+	uint32_t	rsvd2;
+	uint32_t	low_filter_param;
+	uint32_t	high_filter_param;
+	uint32_t	rsvd3;
+	uint32_t	rsvd4;
+	uint32_t	rsvd5;
+	uint32_t	rsvd6;
+};
+
+#endif
