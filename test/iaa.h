343b8c16fd4abcec3acfaa2f2729499bcb893820
Zhang, Li4 <li4.zhang@intel.com>
2022-09-01 17:51:33 +0800

    accel-config/test: Add test code of operation Set membership
    Add test code of operation Set membership into IAA test.
    
    Signed-off-by: Li Zhang <li4.zhang@intel.com>
    Reviewed-by: Fenghua Yu <fenghua.yu@intel.com>

--- test/iaa.h
+++ test/iaa.h
@@ -18,6 +18,7 @@ int iaa_zdecompress32_multi_task_nodes(struct acctest_context *ctx);
 int iaa_compress_multi_task_nodes(struct acctest_context *ctx);
 int iaa_decompress_multi_task_nodes(struct acctest_context *ctx);
 int iaa_scan_multi_task_nodes(struct acctest_context *ctx);
+int iaa_set_membership_multi_task_nodes(struct acctest_context *ctx);
 
 void iaa_prep_noop(struct task *tsk);
 void iaa_prep_crc64(struct task *tsk);
@@ -28,6 +29,7 @@ void iaa_prep_zdecompress32(struct task *tsk);
 void iaa_prep_compress(struct task *tsk);
 void iaa_prep_decompress(struct task *tsk);
 void iaa_prep_scan(struct task *tsk);
+void iaa_prep_set_membership(struct task *tsk);
 
 int iaa_task_result_verify(struct task *tsk, int mismatch_expected);
 int iaa_task_result_verify_task_nodes(struct acctest_context *ctx, int mismatch_expected);
@@ -39,5 +41,6 @@ int task_result_verify_zdecompress32(struct task *tsk, int mismatch_expected);
 int task_result_verify_compress(struct task *tsk, int mismatch_expected);
 int task_result_verify_decompress(struct task *tsk, int mismatch_expected);
 int task_result_verify_scan(struct task *tsk, int mismatch_expected);
+int task_result_verify_set_membership(struct task *tsk, int mismatch_expected);
 
 #endif
