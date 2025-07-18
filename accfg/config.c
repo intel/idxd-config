intel/idxd-config

commit ac8294cc815b27bd18183829b2b5fcc14591bd66

static-analysis: Fix coverity memory leak issues
Fix some memory leak issues in config.c and list.c

Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>

accfg/config.c
@@ -1111,6 +1111,11 @@ static bool filter_device(struct accfg_device *device,
 
 	jc->jgroup_assigned = calloc(max_groups,
 			sizeof(struct json_object *));
+
+	jc->jgroup_assigned = NULL;
+	jc->jwq_group = NULL;
+	jc->jengine_group = NULL;
+
 	if (!jc->jgroup_assigned)
 		goto err;
 
@@ -1140,6 +1145,12 @@ static bool filter_device(struct accfg_device *device,
 	return true;
 
 err:
+	if (jc->jgroup_assigned)
+		free(jc->jgroup_assigned);
+	if (jc->jwq_group)
+		free(jc->jwq_group);
+	if (jc->jengine_group)
+		free(jc->jengine_group);
 	free(jc);
 	return false;
 }

accfg/list.c
@@ -408,6 +408,10 @@ static bool filter_device(struct accfg_device *device,
 	if (!jc)
 		return false;
 
+	jc->jgroup_assigned = NULL;
+	jc->jwq_group = NULL;
+	jc->jengine_group = NULL;
+
 	jc->jgroup_assigned = calloc(max_groups, sizeof(struct json_object *));
 	if (!jc->jgroup_assigned)
 		goto err_jc;
@@ -438,6 +442,13 @@ static bool filter_device(struct accfg_device *device,
 
 	return true;
 err_jc:
+	if (jc->jgroup_assigned)
+		free(jc->jgroup_assigned);
+	if (jc->jwq_group)
+		free(jc->jwq_group);
+	if (jc->jengine_group)
+		free(jc->jengine_group);
+
 	free(jc);
 	return false;
 }

commit ec256f2b088800edcbaf88bf8d72a05ec40ce244

Fix null setting of jc->jgroup_assigned
The pointer jc->jgroup_assigned is being assigned a NULL immediately
after it was assigned the return from a calloc call. This is incorrect,
it causes a memory leak and causes the filter_device function to fail.
Fix this by removing this erroneous statement.

Fixes: ac8294cc815b ("static-analysis: Fix coverity memory leak issues")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

accfg/config.c
@@ -1112,7 +1112,6 @@ static bool filter_device(struct accfg_device *device,
 	jc->jgroup_assigned = calloc(max_groups,
 			sizeof(struct json_object *));
 
-	jc->jgroup_assigned = NULL;
 	jc->jwq_group = NULL;
 	jc->jengine_group = NULL;
 